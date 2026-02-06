00f2cff2b169628d2a8cadf0752dc42435eb6309
Sergio Oropeza Hernandez <ser.oro.hdz.14@gmail.com>
2026-02-05 20:12:34 -0600

    se modifico el archivo registraralumno.php

--- registraralumno.php
+++ registraralumno.php
@@ -231,6 +231,7 @@ $conn->close();
         <h1>👨‍🎓 <?php echo $alumno_editar ? 'Editar' : 'Registrar'; ?> Alumno</h1>
         
         <div class="navegacion">
+            <a href="configurar_catalogos.php" class="nav-link">Catálogo Carreras</a>
             <a href="registrar_grupo.php" class="nav-link">Registrar Grupo</a>
             <a href="registrar_alumno.php" class="nav-link active">Registrar Alumno</a>
             <a href="alumnos_registrados.php" class="nav-link">Ver Alumnos</a>
